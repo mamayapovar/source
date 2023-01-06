bad7c9cf272705a7e0ec89d598562a3ab27a8a8d
lostsleepyboii <lostsleepyboii@gmail.com>
2023-01-07 02:53:59 +0500

    фикс багов при использовании клавиатуры

--- src/js/components/_menu-more.js
+++ src/js/components/_menu-more.js
@@ -1,42 +1,125 @@
-import vars from '../_vars';
-
 (function(){
-  if (document.querySelector('[data-more-menu]')) {
-		vars.bodyEl.addEventListener('click', (e) => {
-			const target = e.target
-			if (target.closest('[data-more-menu-toggle]')) {
-				const moreToggle = target;
-				const moreMenu = target.nextElementSibling;
+  if (document.querySelector('[data-menu-more]')) {
+		const menuMore = document.querySelector('[data-menu-more]')
+		let optionsList = Array.from(menuMore.children)
+		let optionsCount = optionsList.length;
+		let optionHoveredIndex = -1;
 
-				moreToggle.classList.toggle('active')
-				moreMenu.classList.toggle('active')
+		function openMenu(menuMoreToggle, menuMore) {
+			menuMoreToggle.classList.toggle('active')
+			menuMore.classList.toggle('active')
 
-				if (moreMenu.classList.contains('active')) {
-					moreToggle.setAttribute('aria-expanded', 'true');
-					moreToggle?.setAttribute('aria-label', 'Закрыть меню действий');
-				} else {
-					moreToggle.setAttribute('aria-expanded', 'false');
-					moreToggle.setAttribute('aria-label', 'Открыть меню действий');
-				}
+			if (menuMore.classList.contains('active')) {
+				menuMoreToggle.setAttribute('aria-expanded', 'true');
+				menuMoreToggle.setAttribute('aria-label', 'Закрыть меню действий');
+				updateOption(optionHoveredIndex, menuMore)
+			} else {
+				menuMoreToggle.setAttribute('aria-expanded', 'false');
+				menuMoreToggle.setAttribute('aria-label', 'Открыть меню действий');
 			}
+		}
+
+		function closeMenu(menuMoreToggle, menuMore) {
+			menuMoreToggle.forEach((e) => {
+				e.setAttribute('aria-expanded', 'false');
+				e.setAttribute('aria-label', 'Открыть меню действий');
+				e.classList.remove('active')
+			})
+
+			menuMore.forEach((e) => {
+				e.classList.remove('active');
+			})
+		}
+
+		function updateOption(newIndex, menuMore) {
+			optionsList = Array.from(menuMore.children)
+			optionsCount = optionsList.length - 1
+			const prevOption = menuMore.children[optionHoveredIndex];
+			const option = menuMore.children[newIndex];
+
+			if (prevOption) {
+				prevOption.classList.remove("focused");
+			}
+
+			if (option) {
+				option.classList.add("focused");
+			}
+
+			if (option != optionsList[optionsCount]) {
+				optionsList[optionsCount].classList.remove('focused')
+			}
+
+			optionHoveredIndex = newIndex;
+		}
+
+		function supportKeyboardNavigation(e, menuMoreToggle, menuMore) {
+			menuMore.forEach((menu) => {
+				if (e.key == "ArrowDown" && menu.classList.contains('active') && optionHoveredIndex < optionsCount) {
+					e.preventDefault();
+					updateOption(optionHoveredIndex + 1, menu);
+				} else if (e.key == "ArrowDown" && menu.classList.contains('active') && optionHoveredIndex >= optionsCount) {
+					e.preventDefault();
+					optionHoveredIndex = 0;
+					updateOption(optionHoveredIndex, menu);
+				}
+
+				if (e.key == "ArrowUp" && menu.classList.contains('active') && optionHoveredIndex > 0) {
+					e.preventDefault();
+					updateOption(optionHoveredIndex - 1, menu);
+				} else if (e.key == "ArrowUp" && menu.classList.contains('active') && optionHoveredIndex <= 0) {
+					e.preventDefault();
+					updateOption(optionsCount, menu);
+				}
+
+				if (e.key == "Escape" || e.key == "Tab" && menu.classList.contains('active')) {
+					closeMenu(menuMoreToggle, menuMore)
+				}
+			})
+
+			// if (e.key == "Enter" && menuMore.classList.contains('active')) {
+			// 	e.preventDefault();
+			// 	const option = menuMore.children[optionHoveredIndex];
+			// 	const optionLink = option && option.hasAttribute('data-menu-more-item')
+
+			// 	if (optionLink) {
+			// 		const link = option.querySelector('a')
+			// 		const href = link.getAttribute('href')
+			// 		document.location = href
+			// 	}
+
+			// 	closeMenu(menuMoreToggle, menuMore)
+			// }
+		}
+
+		window.addEventListener('keydown', (e) => {
+			const menuMoreToggle = document.querySelectorAll('[data-menu-more-toggle]')
+			const menuMore = document.querySelectorAll('[data-menu-more]')
+			supportKeyboardNavigation(e, menuMoreToggle, menuMore)
 		})
 
-    vars.bodyEl.addEventListener('click', (e) => {
-      const target = e.target
-      if (!target.closest('[data-more-menu-toggle]') && !target.closest('[data-more-menu]')) {
-				const moreToggle = document.querySelectorAll('[data-more-menu-toggle]')
-				const moreMenu = document.querySelectorAll('[data-more-menu]')
+		document.addEventListener('click', (e) => {
+			const target = e.target
 
-				moreToggle.forEach((e) => {
-					e.setAttribute('aria-expanded', 'false');
-					e.setAttribute('aria-label', 'Открыть меню действий');
-					e.classList.remove('active')
-				})
+			if (target.closest('[data-menu-more-toggle]')) {
+				const menuMoreToggle = target;
+				const menuMore = target.nextElementSibling;
 
-				moreMenu.forEach((e) => {
-					e.classList.remove('active');
-				})
+				openMenu(menuMoreToggle, menuMore)
+			}
+
+			if (target.closest('[data-menu-more-item]')) {
+				const menuMoreToggle = document.querySelectorAll('[data-menu-more-toggle]')
+				const menuMore = document.querySelectorAll('[data-menu-more]')
+
+				closeMenu(menuMoreToggle, menuMore)
+			}
+
+			if (!target.closest('[data-menu-more-toggle]') && !target.closest('[data-menu-more]')) {
+				const menuMoreToggle = document.querySelectorAll('[data-menu-more-toggle]')
+				const menuMore = document.querySelectorAll('[data-menu-more]')
+
+				closeMenu(menuMoreToggle, menuMore)
       }
-    });
+		})
   }
 })();
--- src/js/components/_menu-profile.js
+++ src/js/components/_menu-profile.js
@@ -1,9 +1,78 @@
 (function(){
-  const menuProfileToggle = document.querySelector('[data-menu-profile-toggle]');
-  const menuProfile = document.querySelector('[data-menu-profile]');
-  const menuProfileLinks = document.querySelectorAll('[data-menu-profile-link]');
+  if (document.querySelector('[data-menu-profile-toggle]')) {
+		const menuProfileToggle = document.querySelector('[data-menu-profile-toggle]');
+		const menuProfile = document.querySelector('[data-menu-profile]');
+		const menuProfileLinks = document.querySelectorAll('[data-menu-profile-link]');
 
-  if (menuProfileToggle) {
+		let optionsList = Array.from(menuProfile.children)
+		let optionsCount = optionsList.length;
+		let optionHoveredIndex = -1;
+
+		function closeMenu() {
+			menuProfileToggle.setAttribute('aria-expanded', 'false');
+			menuProfileToggle.setAttribute('aria-label', 'Открыть меню');
+			menuProfileToggle.classList.remove('active')
+			menuProfile.classList.remove('active');
+		}
+
+		function updateOption(newIndex) {
+			optionsList = Array.from(menuProfile.children)
+			optionsCount = optionsList.length - 1
+			const prevOption = menuProfile.children[optionHoveredIndex];
+			const option = menuProfile.children[newIndex];
+
+			if (prevOption) {
+				prevOption.classList.remove("focused");
+			}
+
+			if (option) {
+				option.classList.add("focused");
+			}
+
+			if (option != optionsList[optionsCount]) {
+				optionsList[optionsCount].classList.remove('focused')
+			}
+
+			optionHoveredIndex = newIndex;
+		}
+
+		function supportKeyboardNavigation(e) {
+			if (e.key == "ArrowDown" && menuProfile.classList.contains('active') && optionHoveredIndex < optionsCount) {
+				e.preventDefault();
+				updateOption(optionHoveredIndex + 1);
+			} else if (e.key == "ArrowDown" && menuProfile.classList.contains('active') && optionHoveredIndex >= optionsCount) {
+				e.preventDefault();
+				optionHoveredIndex = 0;
+				updateOption(optionHoveredIndex);
+			}
+
+			if (e.key == "ArrowUp" && menuProfile.classList.contains('active') && optionHoveredIndex > 0) {
+				e.preventDefault();
+				updateOption(optionHoveredIndex - 1);
+			} else if (e.key == "ArrowUp" && menuProfile.classList.contains('active') && optionHoveredIndex <= 0) {
+				e.preventDefault();
+				updateOption(optionsCount);
+			}
+
+			if (e.key == "Enter" && menuProfile.classList.contains('active')) {
+				e.preventDefault();
+				const option = menuProfile.children[optionHoveredIndex];
+				const optionLink = option && option.hasAttribute('data-menu-profile-link')
+
+				if (optionLink) {
+					const link = option.querySelector('a')
+					const href = link.getAttribute('href')
+					document.location = href
+				}
+
+				closeMenu()
+			}
+
+			if (e.key == "Escape" || e.key == "Tab" && menuProfile.classList.contains('active')) {
+				closeMenu()
+			}
+		}
+
     menuProfileToggle.addEventListener('click', () => {
 			menuProfileToggle.classList.toggle('active')
       menuProfile.classList.toggle('active');
@@ -11,45 +80,24 @@
       if (menuProfile.classList.contains('active')) {
         menuProfileToggle.setAttribute('aria-expanded', 'true');
         menuProfileToggle.setAttribute('aria-label', 'Закрыть меню');
+				window.addEventListener('keydown', supportKeyboardNavigation)
+				updateOption(optionHoveredIndex);
       } else {
         menuProfileToggle.setAttribute('aria-expanded', 'false');
         menuProfileToggle.setAttribute('aria-label', 'Открыть меню');
+				window.removeEventListener('keydown', supportKeyboardNavigation)
       }
     });
 
-    window.addEventListener('click', (e) => {
+    document.addEventListener('click', (e) => {
       const target = e.target
-      if (!target.closest('[data-menu-profile-toggle]') && !target.closest('[data-menu-profile]')) {
-        menuProfileToggle.setAttribute('aria-expanded', 'false');
-        menuProfileToggle.setAttribute('aria-label', 'Открыть меню');
-				menuProfileToggle.classList.remove('active')
-        menuProfile.classList.remove('active');
+      if (!target.closest('[data-menu-profile-toggle]') && !target.closest('[data-menu-profile]') && menuProfile.classList.contains('active')) {
+        closeMenu()
       }
     });
-
-		// window.addEventListener('keydown', (e) => {
-		// 	if (e.keyCode == 27 && menuProfile.classList.contains('active')) {
-		// 		menuProfileToggle.setAttribute('aria-expanded', 'false');
-		// 		menuProfileToggle.setAttribute('aria-label', 'Открыть меню');
-		// 		menuProfileToggle.classList.remove('active')
-    //     menuProfile.classList.remove('active');
-		// 	}
-
-		// 	if (e.which == 9 && menuProfile.classList.contains('active')) {
-		// 		menuProfileToggle.setAttribute('aria-expanded', 'false');
-		// 		menuProfileToggle.setAttribute('aria-label', 'Открыть меню');
-		// 		menuProfileToggle.classList.remove('active')
-    //     menuProfile.classList.remove('active');
-		// 	}
-		// });
 
     menuProfileLinks.forEach(el => {
-      el.addEventListener('click', () => {
-        menuProfileToggle.setAttribute('aria-expanded', 'false');
-        menuProfileToggle.setAttribute('aria-label', 'Открыть меню');
-				menuProfileToggle.classList.remove('active')
-        menuProfile.classList.remove('active');
-      });
+      el.addEventListener('click', closeMenu);
     });
   }
 })();
