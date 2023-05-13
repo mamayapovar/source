46e5aedad85581a641738e939f5170f86dde2b4c
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2023-05-14 00:24:20 +0500

    откатил публичные фишки

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,7 +1,7 @@
-import './components/_bookmark.js';
-import './components/_like.js';
-import './components/_subscribe.js';
-import './components/_filter.js';
+// import './components/_bookmark.js';
+// import './components/_like.js';
+// import './components/_subscribe.js';
+// import './components/_filter.js';
 import './components/_menu-profile.js';
 import './components/_short-ingredients.js';
 import './components/_menu-more.js';
@@ -14,7 +14,7 @@ import './components/_counter.js'
 import './components/_input-validate.js';
 import './components/_textarea-resize.js';
 import './components/_info-avatar.js';
-import './components/_search-query.js';
-// import './components/_search-query-server.js';
+// import './components/_search-query.js';
+import './components/_search-query-server.js';
 import './components/_edit.js';
 
